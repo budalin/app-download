6b5ed4b880f23d4c1ef6cb5f69df4e3ab71661be
Htun Lin Aung <htunlinnaung@amdon.com>
2018-04-02 12:18:55 +0630

    Download app build with react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {render} from 'react-dom';
 import Header from './components/header/Header';
 import Applist from './components/applist/Applist';
 import './App.css';
--- src/components/applist/Applist.js
+++ src/components/applist/Applist.js
@@ -1,15 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {withStyles} from 'material-ui/styles';
-import Paper from 'material-ui/Paper';
+// import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
-import Card, {CardActions, CardHeader, CardContent, CardMedia} from 'material-ui/Card';
+import Card, {CardActions, CardHeader, CardContent} from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import Divider from 'material-ui/Divider';
-import Icon from 'material-ui/Icon';
-import Save from 'material-ui-icons/Save';
+// import Icon from 'material-ui/Icon';
+// import Save from 'material-ui-icons/Save';
 import FileDownload from 'material-ui-icons/FileDownload';
 import './Applist.css';
+
+const ReactMarkdown = require('react-markdown');
 
 const styles = theme => ({
     root: {
@@ -43,7 +45,9 @@ class Applist extends React.Component {
     constructor(props) {
         super(props)
         this.classes = styles
-        this.applist = [];
+        this.state = {
+            applist: []
+        }
     }
 
     componentDidMount() {
@@ -57,146 +61,74 @@ class Applist extends React.Component {
             .then(response => {
                 console.log('Success:', response)
                 this.applist = response;
+                this.setState({applist: response})
             });
     }
 
+    checkTitle = (title) => {
+        console.log(title)
+        if (title === 'win') {
+            return 'Window'
+        } else if (title === 'android') {
+            return 'Android'
+        } else if (title === 'mac') {
+            return 'Mac'
+        } else if (title === 'ios') {
+            return 'Ios'
+        }
+    }
+
+    checkVersion = (version) => {
+        return 'Version : ' + version
+    }
+
     render() {
+
+        const content = this
+            .state
+            .applist
+            .map((app, index) => <Grid key={index} className="Applist-card" item xs={12} sm={6} md={3} lg={3}>
+                <Card className={this.classes.card}>
+                    <CardHeader
+                        className="Applist-header"
+                        title={this.checkTitle(app.os)}
+                        subheader={this.checkVersion(app.version)}/>
+                    <Divider/>
+                    <CardContent className="Applist-content">
+                        <Typography>
+                            <p className="Applist-title">
+                                {app.requirement
+                                        ? app.requirement.title
+                                        : ''}
+                            </p>
+                            <p>
+                                {app.requirement
+                                    ? <ReactMarkdown source={app.requirement.description}/>
+                                    : ''}
+                            </p>
+                            <p className="Applist-title">
+                                Change Logs
+                            </p>
+                            <p>
+                                <ReactMarkdown source={app.changelog}/>
+                            </p>
+                        </Typography>
+                    </CardContent>
+                    <Divider/>
+                    <CardActions>
+                        <Grid container alignItems="center" direction="row" justify="center">
+                            <Button variant="raised" color="primary" href={app.url}>
+                                <FileDownload className={this.classes.rightIcon}/>
+                                Download App
+                            </Button>
+                        </Grid>
+                    </CardActions>
+                </Card>
+            </Grid>);
         return (
             <div className="Applist-wrapper">
                 <Grid container spacing={16} alignItems="center">
-                    <Grid className="Applist-card" item xs={12} sm={6} md={3} lg={3}>
-                        <Card className={this.classes.card}>
-                            <CardHeader
-                                className="Applist-header"
-                                title="Get ANAVO for Windows Desktop"
-                                subheader=""/>
-                            <Divider/>
-                            <CardContent className="Applist-content">
-                                <Typography component="p">
-                                    <p>
-                                        <strong>Minimum System Requirements for Window</strong>
-                                    </p>
-                                    <p>- Intel Pentium Class Core 2 duo 3rd gen and above. AMD Bulldozer Core
-                                        Microprocessor and above
-                                    </p>
-                                    <p>- Windows 7 or later. 32bit or 64 bit(older OS versions may work but are not
-                                        officially supported)
-                                    </p>
-                                    <p>- Minimum 100 MB available disk space (for installation only)
-                                    </p>
-                                    <p>- Minimum resolution 1024 by 768</p>
-                                    <p>
-                                        <strong>Change Logs</strong>
-                                    </p>
-                                    <p>- Intel Pentium Class Core 2 duo 3rd gen and above. AMD Bulldozer Core
-                                        Microprocessor and above
-                                    </p>
-                                    <p>- Windows 7 or later. 32bit or 64 bit(older OS versions may work but are not
-                                        officially supported)
-                                    </p>
-                                    <p>- Minimum 100 MB available disk space (for installation only)
-                                    </p>
-                                    <p>- Minimum resolution 1024 by 768</p>
-                                </Typography>
-                            </CardContent>
-                            <Divider/>
-                            <CardActions>
-                                <Grid container alignItems="center" direction="row" justify="center">
-                                    <Button variant="raised" color="primary">
-                                        <FileDownload className={this.classes.rightIcon}/>
-                                        Download
-                                    </Button>
-                                </Grid>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid className="Applist-card" item xs={12} sm={6} md={3} lg={3}>
-                        <Card className={this.classes.card}>
-                            <CardHeader
-                                className="Applist-header"
-                                title="Get ANAVO for Android Tablets"
-                                subheader=""/>
-                            <Divider/>
-                            <CardContent className="Applist-content">
-                                <Typography component="p">
-                                    <p>
-                                        <strong>Minimum System Requirements for Window</strong>
-                                    </p>
-                                    <p>- Android smartphone(2014 and newer).</p>
-                                    <p>- Android 4.4 or later.(older OS versions may work but are not officially
-                                        supported)</p>
-                                    <p>- 20MB of storage for base installation + additional storage required for
-                                        content</p>
-                                    <p>- Phone:4.5 inch screen size and above Tablet: 8 inch screen size and above</p>
-                                </Typography>
-                            </CardContent>
-                            <Divider/>
-                            <CardActions>
-                                <Grid container alignItems="center" direction="row" justify="center">
-                                    <Button variant="raised" color="primary">
-                                        <FileDownload className={this.classes.rightIcon}/>
-                                        Download
-                                    </Button>
-                                </Grid>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid className="Applist-card" item xs={12} sm={6} md={3} lg={3}>
-                        <Card className={this.classes.card}>
-                            <CardHeader className="Applist-header" title="Get ANAVO for OSX" subheader=""/>
-                            <Divider/>
-                            < CardContent className="Applist-content">
-                                <Typography component="p">
-                                    <p>
-                                        <strong>Minimum System Requirements for Window</strong>
-                                    </p>
-                                    <p>- iMac, Macbook family (2012 and newer)</p>
-                                    <p>- OSX 10.6 and above (older OS versions may work but are not officially
-                                        supported)
-                                    </p>
-                                    <p>- Minimum 200 MB available disk space (for installation only)</p>
-                                    <p>- Minimum resolution 1024 by 768</p>
-                                </Typography>
-                            </CardContent>
-                            <Divider/>
-                            <CardActions>
-                                <Grid container alignItems="center" direction="row" justify="center">
-                                    <Button variant="raised" color="primary">
-                                        <FileDownload className={this.classes.rightIcon}/>
-                                        Download
-                                    </Button>
-                                </Grid>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid className="Applist-card" item xs={12} sm={6} md={3} lg={3}>
-                        <Card className={this.classes.card}>
-                            <CardHeader className="Applist-header" title="Get ANAVO for iPad" subheader=""/>
-                            <Divider/>
-                            <CardContent className="Applist-content">
-                                <Typography component="p">
-                                    <p>
-                                        <strong>Minimum System Requirements for Window</strong>
-                                    </p>
-                                    <p>- iPhone 6. iPhone 6 plus & above. iPad family (3rd generation and above)
-                                    </p>
-                                    <p>- iOS 10 or later.(older OS versions may work but are not officially supported)
-                                    </p>
-                                    <p>- 20MB of storage for installation+ additional storage required for content</p>
-                                    <p>- Phone: 4.7 inch screen size and above Tablet: 7.9 inch screen size and above</p>
-                                </Typography>
-                            </CardContent>
-                            <Divider/>
-                            <CardActions>
-                                <Grid container alignItems="center" direction="row" justify="center">
-                                    <Button variant="raised" color="primary">
-                                        <FileDownload className={this.classes.rightIcon}/>
-                                        Download
-                                    </Button>
-                                </Grid>
-                            </CardActions>
-                        </Card>
-                    </Grid>
+                    {content}
                 </Grid>
             </div>
         )
